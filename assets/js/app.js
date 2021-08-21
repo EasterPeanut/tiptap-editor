5a34f2b6057abd66735dcb132f7047f49dc5d34b
EasterPeanut <pien_van_dalen@hotmail.com>
2021-08-21 14:51:17 +0200

    Move editor setup code to editor.js
    Separated code for cleaner code architecture.

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,9 +23,7 @@ import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import Alpine from 'alpinejs'
 import topbar from '../vendor/topbar'
-// import editor from '../vendor/editor'
-import { Editor } from '@tiptap/core'
-import StarterKit from '@tiptap/starter-kit'
+import setupEditor from '../vendor/editor'
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
 const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
@@ -37,52 +35,7 @@ window.addEventListener('phx:page-loading-stop', info => topbar.hide())
 
 // Tiptap editor
 document.addEventListener('alpine:init', () => {
-  Alpine.data('editor', (content) => {
-    let editor // eslint-ignore-line prefer-const
-
-    return {
-      // Passing updatedAt here to make Alpine rerender the menu buttons.
-      // The value of updatedAt will be updated on every Tiptap transaction.
-      isActive (type, opts = {}, updatedAt) {
-        return editor.isActive(type, opts)
-      },
-      toggleBold () {
-        editor.chain().toggleBold().focus().run()
-      },
-      toggleItalic () {
-        editor.chain().toggleItalic().focus().run()
-      },
-      toggleHeading (level) {
-        editor.chain().toggleHeading({ level: level }).focus().run()
-      },
-      updatedAt: Date.now(),
-      init () {
-        const _this = this
-
-        editor = new Editor({
-          element: this.$refs.editorReference,
-          extensions: [
-            StarterKit.configure({
-              heading: {
-                levels: [1, 2, 3]
-              },
-              mention: false
-            })
-          ],
-          content: content,
-          onCreate ({ editor }) {
-            _this.updatedAt = Date.now()
-          },
-          onUpdate ({ editor }) {
-            _this.updatedAt = Date.now()
-          },
-          onSelectionUpdate ({ editor }) {
-            _this.updatedAt = Date.now()
-          }
-        })
-      }
-    }
-  })
+  Alpine.data('editor', (content) => setupEditor(content))
 })
 
 // Start Alpine.js
--- assets/vendor/editor.js
+++ assets/vendor/editor.js
@@ -1,23 +1,49 @@
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
 
-export default function (element, content) {
-  return new Editor({
-    element: element,
-    extensions: [
-      StarterKit.configure({
-        heading: {
-          levels: [1, 2, 3]
-        },
-        mention: false
-      })
-    ],
-    content: this.content,
-    onUpdate: ({ editor }) => {
-      this.content = editor.getHTML()
+export default function (content) {
+  let editor
+
+  return {
+    // Passing updatedAt here to make Alpine rerender the menu buttons.
+    // The value of updatedAt will be updated on every Tiptap transaction.
+    isActive (type, opts = {}, updatedAt) {
+      return editor.isActive(type, opts)
     },
-    onSelectionUpdate: () => {
-      this.updatedAt = Date.now()
+    toggleBold () {
+      editor.chain().toggleBold().focus().run()
+    },
+    toggleItalic () {
+      editor.chain().toggleItalic().focus().run()
+    },
+    toggleHeading (level) {
+      editor.chain().toggleHeading({ level: level }).focus().run()
+    },
+    updatedAt: Date.now(),
+    init () {
+      const _this = this
+
+      editor = new Editor({
+        element: this.$refs.editorReference,
+        extensions: [
+          StarterKit.configure({
+            heading: {
+              levels: [1, 2, 3]
+            },
+            mention: false
+          })
+        ],
+        content: content,
+        onCreate ({ editor }) {
+          _this.updatedAt = Date.now()
+        },
+        onUpdate ({ editor }) {
+          _this.updatedAt = Date.now()
+        },
+        onSelectionUpdate ({ editor }) {
+          _this.updatedAt = Date.now()
+        }
+      })
     }
-  })
+  }
 }
