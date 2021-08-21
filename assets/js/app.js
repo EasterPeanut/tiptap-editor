907c508ca5cf9d2ea8d09a236eba5a44b55fc66d
EasterPeanut <pien_van_dalen@hotmail.com>
2021-08-21 13:58:36 +0200

    Fix mismatch bug in tiptap/alpine.js
    By not having the editor reactive, only the menu/buttons.

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,9 @@ import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import Alpine from 'alpinejs'
 import topbar from '../vendor/topbar'
-import editor from '../vendor/editor'
+// import editor from '../vendor/editor'
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
 const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
@@ -33,8 +35,59 @@ topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' })
 window.addEventListener('phx:page-loading-start', info => topbar.show())
 window.addEventListener('phx:page-loading-stop', info => topbar.hide())
 
-// Tiptap init
-window.setupEditor = editor
+// Tiptap editor
+document.addEventListener('alpine:init', () => {
+  Alpine.data('editor', (content) => {
+    let editor // eslint-ignore-line prefer-const
+
+    return {
+      // Passing updatedAt here to make Alpine rerender the menu buttons.
+      // The value of updatedAt will be updated on every Tiptap transaction.
+      isActive (type, opts = {}, updatedAt) {
+        return editor.isActive(type, opts)
+      },
+      toggleBold () {
+        editor.chain().toggleBold().focus().run()
+      },
+      toggleItalic () {
+        editor.chain().toggleItalic().focus().run()
+      },
+      toggleHeading (level) {
+        editor.chain().toggleHeading({ level: level }).focus().run()
+      },
+      updatedAt: Date.now(),
+      init () {
+        const _this = this
+
+        editor = new Editor({
+          element: this.$refs.editorReference,
+          extensions: [
+            StarterKit.configure({
+              heading: {
+                levels: [1, 2, 3]
+              },
+              mention: false
+            })
+          ],
+          content: content,
+          onCreate ({ editor }) {
+            _this.updatedAt = Date.now()
+          },
+          onUpdate ({ editor }) {
+            _this.updatedAt = Date.now()
+          },
+          onSelectionUpdate ({ editor }) {
+            _this.updatedAt = Date.now()
+          }
+        })
+      }
+    }
+  })
+})
+
+// Start Alpine.js
+window.Alpine = Alpine
+Alpine.start()
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -44,5 +97,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.LiveSocket = LiveSocket
-window.Alpine = Alpine
-Alpine.start()
--- assets/vendor/editor.js
+++ assets/vendor/editor.js
@@ -1,31 +1,23 @@
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
 
-export default function (content) {
-  const _window = this
-
-  return {
-    content: content,
-    updatedAt: Date.now(), // force Alpine to rerender on selection change
-    init (element) {
-      _window.editor = new Editor({
-        element: element,
-        extensions: [
-          StarterKit.configure({
-            heading: {
-              levels: [1, 2, 3]
-            },
-            mention: false
-          })
-        ],
-        content: this.content,
-        onUpdate: ({ editor }) => {
-          this.content = editor.getHTML()
+export default function (element, content) {
+  return new Editor({
+    element: element,
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          levels: [1, 2, 3]
         },
-        onSelectionUpdate: () => {
-          this.updatedAt = Date.now()
-        }
+        mention: false
       })
+    ],
+    content: this.content,
+    onUpdate: ({ editor }) => {
+      this.content = editor.getHTML()
+    },
+    onSelectionUpdate: () => {
+      this.updatedAt = Date.now()
     }
-  }
+  })
 }
